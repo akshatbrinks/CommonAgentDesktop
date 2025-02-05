93f5df5eeea3d732e6ce34c0ec28090c71872591
Akshat Agarwal <akshat.agarwal@cginfinity.com>
2025-02-05 19:00:54 +0530

    first commit

---
+++ CommonAgentDesktop.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonAgentDesktop.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CommonAgentDesktop.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CommonAgentDesktop.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
